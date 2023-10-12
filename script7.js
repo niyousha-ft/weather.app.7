dd060ac16ce183eff981a28f9e65342c3262c862
niyousha-ft <niyoushaft@gmail.com>
2023-10-12 15:53:50 +0330

    Updated github and JS

--- script7.js
+++ script7.js
@@ -89,4 +89,4 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
-search("New York");
+search("Tehran");
